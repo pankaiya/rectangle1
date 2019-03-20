dbe0792387040972ba8edc67b0d8111aa2cee875
pankaiya <3131225624@qq.com>
2019-03-20 18:26:13 +0800

    v0.3 unit test

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,4 @@
+/* global Rectangle: true */
 $(function() {
   var $width = $('#width'),
       $height = $('#height'),
@@ -9,10 +10,9 @@ $(function() {
     var w = Number($width.val()),
         h = Number($height.val());
 
-    var p = 2 * (w + h),
-        a = w * h;
+    var r = new Rectangle(w,h);
 
-    $perimeter.val(p);
-    $area.val(a);              
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());              
   }); 
 });
