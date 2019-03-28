afba26fcb7e75798f477b773824662a83c559eaa
pankaiya <3131225624@qq.com>
2019-03-28 10:08:39 +0800

    v1.0 field validation

--- calc.js
+++ calc.js
@@ -11,3 +11,35 @@ function Rectangle(width, height) {
     return 2 * (w + h);
   };    
 }
+
+/**
+ * 对数据进行合法性校验
+ * @param msg 被验证的消息
+ * @returns result 有两个字段
+ *          isOK boolean验证通过为true,验证不通过为false
+ *          reason验证不通过的理由
+ **/
+function validate(data){
+  var result = {
+    isOK: false,
+    reason:''
+  };
+
+  if(data ===''){
+    result.reason = '不能为空！';
+    return result;
+  }
+
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)){
+    result.reason = '必须是数值';
+    return result;
+  } 
+
+  if(Number(data)<0){
+    result.reason = '必须大于零';
+    return result;
+  }
+
+  result.isOK=true;
+  return result;
+}
--- rectangle.js
+++ rectangle.js
@@ -4,15 +4,43 @@ $(function() {
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area');
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
+
+  $height.focusout(function(){
+    var result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidate.html('');
+    }
+  });
+
+  $width.focusout(function(){
+    var result = validate($width.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidate.html('');
+    }
+  });
 
   $btnCal.click(function(){
-    var w = Number($width.val()),
-        h = Number($height.val());
+    if(!isPassValidate) return;
+
+    var w = $width.val(),
+        h = $height.val();
 
     var r = new Rectangle(w,h);
 
     $perimeter.val(r.perimeter());
     $area.val(r.area());              
   }); 
+
 });
